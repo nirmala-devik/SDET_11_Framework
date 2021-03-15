312661b3ac61a6479c5264f33ed522bd1e5a2c09
HP <HP@divya>
2021-03-15 12:58:34 +0530

    commit

---
+++ Comcast_1/src/main/java/com/comcast/contacttest/Createcontact.java
@@ -0,0 +1,16 @@
+package com.comcast.contacttest;
+
+import org.testng.annotations.Test;
+
+public class Createcontact {
+	@Test
+	public void createcontact()
+	{
+		System.out.println("execute create contact");
+	}
+	@Test
+public void CreateconactwithMoile()
+{
+		System.out.println("execute createcontactwithmobile");
+}
+}
---
+++ Comcast_1/src/test/java/com/comcast/contactTest/Createcontact.java
@@ -0,0 +1,21 @@
+package com.comcast.contactTest;
+
+import org.testng.annotations.Test;
+
+public class Createcontact {
+	@Test
+	public void createcontact()
+	{
+		System.out.println("execute create contact");
+	}
+	@Test
+public void Modifycontact()
+{
+		System.out.println("execute Modify contact");
+}
+	@Test
+	public void Deletecontact()
+	{
+			System.out.println("execute Delete Contact");
+	}
+}
